f1c6af5878211a28b9b49102ffab74436c69f3d0
roulamokhtar <roula_mokhtar2001@yahoo.fr>
2018-03-09 07:49:49 +0100

    chemin images

--- view/template/vueFooter.php
+++ view/template/vueFooter.php
@@ -6,17 +6,20 @@
 
 	
 	<script type="text/javascript" src="common/js/form/jquery.validate.min.js"></script>
-	<!--<script type="text/javascript" src="recherche.js"></script>-->
-	<script type="text/javascript" src="common/js/form/additional-methods.min.js"></script>
+ 	<script type="text/javascript" src="common/js/form/additional-methods.min.js"></script>
 	
 	<?php if($page == "maps"): ?>
-		<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script> <!-- Google Maps -->
+<!--	<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>       -->
+		<!-- Google Maps -->
 		<script src="http://www.google.com/jsapi"></script>
 		<script src="common/js/points.json"></script>
  		<script src="common/js/maps.js"></script>
 	<?php endif; ?>
 	
 	<script src="common/js/script.js"></script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDb4zkM4A_4LPifJAWMQFEgeGVmgBDwt2U&callback=initialize"
+  type="text/javascript"></script>
+//AIzaSyDb4zkM4A_4LPifJAWMQFEgeGVmgBDwt2U
 
   </body>
 </html>
