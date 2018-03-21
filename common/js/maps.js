1c6a0e79cb87f0d850e312b05be35265e3e91f1a
roulamokhtar <roula_mokhtar2001@yahoo.fr>
2018-03-21 14:04:15 +0100

    geo

--- common/js/maps.js
+++ common/js/maps.js
@@ -153,6 +153,24 @@ var markers = [];
 // https://www.dropbox.com/s/ztcevthqkosgkxz/Circonscription5.kmz?dl=0
 //layers[5] = new google.maps.Data();
  var infowindow = new google.maps.InfoWindow();
+var infoWindow = new google.maps.InfoWindow({map:map});
+ if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('vous etes ici.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
    // When the user clicks, open an infowindow
  map.data.addListener('click', function(event) { 
     var commune = event.feature.getProperty("commune");
@@ -248,6 +266,14 @@ var markers = [];
 
 
  
+ 
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
 
   
  
