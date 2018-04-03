faed5d3c5c355f526a1c9753c6ca17a0fa2aeff1
roulamokhtar <roula_mokhtar2001@yahoo.fr>
2018-04-03 08:30:13 +0200

    keep alive

---
+++ common/js/script.js
@@ -0,0 +1,63 @@
+$(function() {
+	// /**********************************************
+	 // * Validation de Formulaire
+	 // **********************************************/
+	 /* __Formulaire Maps
+	
+	$(".formMaps").validate({
+		rules: {
+			habitants: "required",
+			departement: "required"
+		},
+		messages: {
+			habitants: "Veuillez choisir un taux de réalisation.",
+			departement: "Veuillez choisir une commune."	
+		}
+	});
+	*/
+	
+	//__Formulaire de contact
+	// $(".formContact").validate({
+		// rules: {
+			// nom: {
+				// required: true,
+				// minlength: 3
+			// },
+			// prenom: {
+				// required: true,
+				// minlength: 3
+			// },
+			// email: {
+				// required: true,
+				// email: true
+			// },
+			// sujet: {
+				// required: true,
+				// rangelength: [10,100]
+			// },
+			// message: {
+				// required: true,
+				// rangelength: [20,3000]
+			// }
+		// },
+		// messages: {
+			// nom: {
+				// required: "Le champ 'Nom' est requis",
+				// minlength: "Votre nom doit faire au moins {0} caractères"
+			// },
+			// prenom: {
+				// required: "Le champ 'prenom' est requis",
+				// minlength: "Votre prenom doit faire au moins {0} caractères"
+			// },
+			// email: "Veuillez entrer une email valide",
+			// sujet: {
+				// required: "Le champ 'sujet' est requis",
+				// rangelength: "Le sujet doit faire entre {0} et {1} caractères"
+			// },
+			// message: {
+				// required: "Le champ 'message' est requis",
+				// rangelength: "Le message doit faire entre {0} et {1} caractères"
+			// }
+		// }
+	// });
+});
